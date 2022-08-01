c29657cbde6f0ab06ca6ea6cc2939ce9f9e89db4
kerbs <73579653+keru6k@users.noreply.github.com>
2022-08-01 10:29:13 +0800

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -1,22 +1,20 @@
 import random
-import hashlib
-
-chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
-length = 1000
 
 def main():
-    
+    k = input("For? ") #generatedpass [something].txt
+    lengthpass = int(input("Length? ")) #length of the password
     password = []
+
+    chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
+    length = lengthpass
+
     for i in range(length):
         password.append(random.choice(chars))
     random.shuffle(password)
 
     print("the generated password is", "".join(password))
-    f = open("generatedpass.txt", "w")
+    f = open("generatedpass " + k + ".txt", "w")
     f.write("".join(password))
     f.close
-
-
-
 
 main()
