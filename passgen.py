82bce3ed1f8299c51ebc3112b90c18fdad0c178e
kerbs <73579653+keru6k@users.noreply.github.com>
2022-09-08 21:30:19 +0800

    added changes
    - Added choices to remove "L" and "I" to the generated password (less confusing on non-monospaced fonts) 
    - Il. See the difference? I don't. That's why I made this.

--- passgen.py
+++ passgen.py
@@ -1,20 +1,57 @@
+# Password Generator in Python
+# Author: Keru6k
+
+# Changes: Added choices to remove "L" and "I" to the generated password
+# Reason: I almost fucked up my exam time because i can't distinguish the L and I on the password i've generated. (I had to manually type it out)
+# its a dumb reason but hey, someone (or me) might need this in the future lul
+
 import random
+import os
+import time
 
 def main():
+
     k = input("For? ") #generatedpass [something].txt
-    lengthpass = int(input("Length? ")) #length of the password
-    password = []
-
+    lengthpass = int(input("Length? "))
+            
     chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
+    charsNoLi = "0123456789abcdefghjkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
     length = lengthpass
+    agree = "yes"
+    disagree = "no"
 
-    for i in range(length):
-        password.append(random.choice(chars))
-    random.shuffle(password)
+    def choose():
+        choiceForRemv = str(input("remove L and i to be less confusing? [YES]  [NO] "))
+        if (choiceForRemv == agree) or (choiceForRemv.lower() == agree.lower()) or choiceForRemv.upper == agree.upper():
+            password = []
+            for i in range(length):
+                password.append(random.choice(charsNoLi))
+            random.shuffle(password)
 
-    print("the generated password is", "".join(password))
-    f = open("generatedpass " + k + ".txt", "w")
-    f.write("".join(password))
-    f.close
+            print("the generated password is", "".join(password))
+            f = open("generatedpass " + k + ".txt", "w")
+            f.write("".join(password))
+            f.close
+            input("Press any to exit ")
+            exit()
+        elif (choiceForRemv == disagree) or (choiceForRemv.lower() == disagree.lower()) or choiceForRemv.upper == disagree.upper():
+            # args above means that even with different capitalizations like "nO", "NO", and "No" are still considered valid answers
+            password = []
+            for i in range(length):
+                password.append(random.choice(chars))
+            random.shuffle(password)
+
+            print("the generated password is", "".join(password))
+            f = open("generatedpass " + k + ".txt", "w")
+            f.write("".join(password))
+            f.close
+            input("Press any to exit ")
+            exit()
+        else:
+            print("Please choose a valid answer.")
+            time.sleep(2)
+            os.system("cls")
+            choose()
+    choose()
 
 main()
